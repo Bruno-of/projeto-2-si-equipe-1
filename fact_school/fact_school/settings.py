e58551844da50182df23e3e675f7ab4a7051ba6d
Matheus <mme@cesar.school>
2024-12-07 16:58:49 -0300

    Ajuste static

--- fact_school/fact_school/settings.py
+++ fact_school/fact_school/settings.py
@@ -123,8 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
